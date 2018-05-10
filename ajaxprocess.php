bed5378118f573b4ee04513a476a67a9f2a4ba75
soph1ac <chan.soph@gmail.com>
2018-05-10 19:40:08 -0400

    basic

--- ajaxprocess.php
+++ ajaxprocess.php
@@ -1,9 +1,10 @@
 <?php
 //Store data from my form into variables
-$n = $_POST['name'];
-$g = $_POST['gender'];
+$w = $_POST['webtype'];
+//$b = $_POST['b2b'];
+//$z = $_POST['b2c'];
 
-//retrieve the Json file and converted it into php array
+////retrieve the Json file and converted it into php array
 $j = file_get_contents('ppl.json');
 $j = json_decode($j, true);
 
@@ -18,8 +19,8 @@ $i = ++$i;
 // insert my variables into an array stored in a variable called $add
 $add = array(
 	"id" => $i,
-	"name" => $n,
-	"gender" => $g
+	"webtype" => $w,
+//	"gender" => $g
 );
 //print_r ($add);
 
@@ -29,10 +30,10 @@ array_push($j, $add);
 
 // take my updated json array, format it back into Json and Overwrite it into the Json file
 $j = json_encode($j);
-file_put_contents('ppl.json', $j);
+file_put_contents('results.json', $j);
 
 // Store the image on my service
-$t = $_FILES['photo']['tmp_name'];
-$f = "img/$i.jpg";
-move_uploaded_file( $t, $f);
+//$t = $_FILES['photo']['tmp_name'];
+//$f = "img/$i.jpg";
+//move_uploaded_file( $t, $f);
 ?>
